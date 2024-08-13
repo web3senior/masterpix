aa42c384f87f12bb8ed2d5afc5c06b3aea4b77ba
web3senior <atenyun@gmail.com>
2024-08-14 01:35:53 +0330

    Clean up routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.scss'
 import './styles/global.scss'
@@ -9,16 +9,9 @@ import ErrorPage from './error-page'
 import Loading from './routes/components/Loading.jsx'
 const Layout = lazy(() => import('./routes/layout.jsx'))
 const UserLayout = lazy(() => import('./routes/userLayout.jsx'))
-import SplashScreen from './routes/splashScreen.jsx'
 import Home, { loader as homeLoader } from './routes/home.jsx'
 import About from './routes/about.jsx'
-import Pricing from './routes/pricing.jsx'
-import TermsOfService from './routes/terms-of-service.jsx'
-import PrivacyPolicy from './routes/privacy-policy.jsx'
 import Ecosystem from './routes/ecosystem.jsx'
-import Favorite from './routes/favorite.jsx'
-import Tip from './routes/tip.jsx'
-import Dashboard from './routes/dashboard.jsx'
 import Admin from './routes/admin.jsx'
 
 const router = createBrowserRouter([
@@ -47,64 +40,11 @@ const router = createBrowserRouter([
         element: <Ecosystem title={`Ecosystem`} />,
       },
       {
-        path: `favorite`,
-        element: <Favorite title={`Favorite`} />,
-      },
-      {
-        path: `tip`,
-        element: <Tip title={`Tip`} />,
-      },
-      {
-        path: `pricing`,
-        element: <Pricing title={`Pricing`} />,
-      },
-      {
         path: `admin`,
         element: <Admin title={`Admin`} />,
       },
-      {
-        path: `terms-of-service`,
-        element: <TermsOfService title={`Terms Of Service`} />,
-      },
-      {
-        path: `privacy-policy`,
-        element: <PrivacyPolicy title={`Privacy Policy`} />,
-      },
     ],
   },
-  // {
-  //   path: 'user',
-  //   element: (
-  //     <Suspense fallback={<Loading />}>
-  //       <AuthProvider>
-  //         <UserLayout />
-  //       </AuthProvider>
-  //     </Suspense>
-  //   ),
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     {
-  //       index: true,
-  //       element: <Dashboard to={`/dashboard`} replace />,
-  //     },
-  //     {
-  //       path: `dashboard`,
-  //       element: <Dashboard title={`Dashboard`} />,
-  //     },
-  //     {
-  //       path: `brand`,
-  //       element: <Dashboard title={`Brand`} />,
-  //     },
-  //     {
-  //       path: `transfer`,
-  //       element: <Dashboard title={`Transfer`} />,
-  //     },
-  //     {
-  //       path: `map`,
-  //       element: <Dashboard title={`Map`} />,
-  //     },
-  //   ],
-  // },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
